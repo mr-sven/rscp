6e7da0db345452500508525a7c861eff5ce8d87b
mr-sven <sven.fabricius@livediesel.de>
2022-05-19 10:25:34 +0200

    adding readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # rscp
+
+This lib is a Rust based E3/DC RSCP connetor.
+
+ - omit the use of double namings and wrappings
+ - implemented native type parsing without wrapping types
+
+The response bit in the Tag field is ignored due the fact there is no known need to take care on this.
+
+Ths lib is in early stage.
+
+# Usage
+
+```rust
+use rscp::tags;
+use rscp;
+
+let mut c = rscp::Client::new("RSCP_KEY", "RSCP_USER".to_string(), "RSCP_PASSWORD".to_string());
+match c.connect("energy.storage.local", None) {
+    Ok(_) => (),
+    Err(err) => {
+        panic!("Unable to connect: {:?}", err);
+    }
+}
+
+let mut info_frame = Frame::new();
+info_frame.push_item(Item { tag: tags::INFO::SERIAL_NUMBER.into(), data: None } );
+info_frame.push_item(Item { tag: tags::INFO::MAC_ADDRESS.into(), data: None } );
+info_frame.push_item(Item { tag: tags::INFO::SW_RELEASE.into(), data: None } );
+
+match c.send_receive_frame(&info_frame) {
+    Ok(result_frame) => {
+        println!("{}", result_frame.get_item_data::<String>(tags::INFO::SERIAL_NUMBER.into()).unwrap());            
+    },
+    Err(err) => {
+        warn!("Unable send: {:?}", err);
+    }
+}
+
+c.disconnect().unwrap();
+```
--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -2,8 +2,8 @@ use std::io::Write;
 use chrono::Local;
 use env_logger::Builder;
 use log::{info, warn, LevelFilter};
-use rscp::tags;
-use rscp;
+use rscp::{tags, GetItem};
+use rscp::{Frame, Item};
 
 fn setup_logging() {
     Builder::new()
@@ -30,14 +30,28 @@ fn main() {
     let g = tags::TagGroup::from(0);    
     info!("{}", g.tags(1));
 
-    let mut c = rscp::Client::new("efwef", "eefwef".to_string(), "wgwegf".to_string());
-    match c.connect("127.0.0.1", None) {
-        Ok(_) => {
-            c.disconnect().unwrap();
-        },
+    let mut c = rscp::Client::new("RSCP_KEY", "RSCP_USER".to_string(), "RSCP_PASSWORD".to_string());
+    match c.connect("energy.storage.local", None) {
+        Ok(_) => (),
         Err(err) => {
-            warn!("Unable to connect: {:?}", err);
+            panic!("Unable to connect: {:?}", err);
         }
     }
+
+    let mut info_frame = Frame::new();
+    info_frame.push_item(Item { tag: tags::INFO::SERIAL_NUMBER.into(), data: None } );
+    info_frame.push_item(Item { tag: tags::INFO::MAC_ADDRESS.into(), data: None } );
+    info_frame.push_item(Item { tag: tags::INFO::SW_RELEASE.into(), data: None } );
+
+    match c.send_receive_frame(&info_frame) {
+        Ok(result_frame) => {
+            info!("{}", result_frame.get_item_data::<String>(tags::INFO::SERIAL_NUMBER.into()).unwrap());            
+        },
+        Err(err) => {
+            warn!("Unable send: {:?}", err);
+        }
+    }
+
+    c.disconnect().unwrap();
     
 }
